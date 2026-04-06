f10e9c582012a4cceead62a067cc81688325048f
JaamDev <jaamdev@hotmail.com>
2026-04-06 19:03:47 +0200

    feat: add loading component

--- src/components/resource/NewResources.tsx
+++ src/components/resource/NewResources.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router'
 import { toast } from 'sonner'
+import Loading from '../loading/Loading.tsx'
 import { API } from '../../config/config.ts'
 import type { IAPI, INewFeature } from '../../types/types.ts'
 
 export default function NewResources() {
-  const [state, setState] = useState<INewFeature[]>()
+  const [loading, setLoading] = useState<boolean>(true)
+  const [state, setState] = useState<INewFeature[]>([])
 
   useEffect(() => {
     const abortController = new AbortController()
@@ -13,6 +15,7 @@ export default function NewResources() {
 
     const getData = async () => {
       try {
+        setLoading(true)
         const response = await fetch(`${API}/changes`, { signal })
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`)
@@ -26,6 +29,8 @@ export default function NewResources() {
           toast.error('No se pudo recuperar los nuevos recursos.')
           console.error(error)
         }
+      } finally {
+        setLoading(false)
       }
     }
 
@@ -33,26 +38,28 @@ export default function NewResources() {
     return () => abortController.abort()
   }, [])
 
-  return <section
-    className='size-full my-2 m-auto px-2 py-1 max-w-6xl'
-  >
-    <h2 className='my-4 text-xl text-center'>Recientes:</h2>
+  return loading
+    ? <Loading />
+    : state.length !== 0 && <section
+      className='size-full my-2 m-auto px-2 py-1 max-w-6xl'
+    >
+      <h2 className='my-4 text-xl text-center'>Recientes:</h2>
 
-    {
-      state && state.map((data, index) => (
-        <article key={index} className='size-full mb-1 border-b border-white'>
-          <div className='p-1 my-2 grid grid-cols-[6rem_1fr] justify-center items-center gap-1'>
-            <h3 className='text-right'>{new Date(data.date).toLocaleDateString()}:</h3>
-            <ul className='flex flex-row flex-wrap justify-center items-center gap-2'>
-              {data.resources.map(({ id, title }) => (
-                <Link key={id} to={`/${id}`} className='px-2 py-1 rounded bg-midudark hover:bg-midu transition-colors duration-300'>
-                  {title}
-                </Link>
-              ))}
-            </ul>
-          </div>
-        </article>
-      ))
-    }
-  </section>
+      {
+        state.map((data, index) => (
+          <article key={index} className='size-full mb-1 border-b border-white'>
+            <div className='p-1 my-2 grid grid-cols-[6rem_1fr] justify-center items-center gap-1'>
+              <h3 className='text-right'>{new Date(data.date).toLocaleDateString()}:</h3>
+              <ul className='flex flex-row flex-wrap justify-center items-center gap-2'>
+                {data.resources.map(({ id, title }) => (
+                  <Link key={id} to={`/${id}`} className='px-2 py-1 rounded bg-midudark hover:bg-midu transition-colors duration-300'>
+                    {title}
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          </article>
+        ))
+      }
+    </section>
 }
