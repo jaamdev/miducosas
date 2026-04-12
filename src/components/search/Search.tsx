b2c4e42a3610ec81652a9dec9f52ef9f2ffeae3e
JaamDev <jaamdev@hotmail.com>
2026-04-12 17:53:46 +0200

    styles(search): Removed an unnecessary div element.

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -57,59 +57,55 @@ export default function Search({ changeSearch }: Props) {
     changeSearch(textSearch, newCategory)
   }
 
-  return <div
-    className='size-full m-auto px-2 py-1 max-w-6xl'
-  >
-    <form
-      ref={formRef}
-      role='search-form'
-      autoComplete='off'
-      onSubmit={handleSubmit}
-      className='
-      p-2 my-5
+  return <form
+    ref={formRef}
+    role='search-form'
+    autoComplete='off'
+    onSubmit={handleSubmit}
+    className='
+      size-full p-2 m-auto my-5 max-w-6xl
       bg-secondary border border-border rounded-full
       flex flex-row items-center gap-2
     '
+  >
+    <label
+      htmlFor='input-search'
+      className='mx-1'
     >
-      <label
-        htmlFor='input-search'
-        className='mx-1'
-      >
-        <SearchIcon
-          className='size-6'
-        />
-      </label>
-      <input
-        id='input-search'
-        type='search'
-        name={idSearch}
-        placeholder='Buscar recursos...'
-        value={textSearch}
-        onChange={handleSearch}
-        className='w-full outline-0'
+      <SearchIcon
+        className='size-6'
       />
-      <select
-        id='input-select'
-        name={idCategory}
-        value={selectedCategory}
-        onChange={handleCategory}
-        className='category-select'
-      >
-        {Object.values(Category).map(option => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-      <input
-        type='submit'
-        value='Buscar'
-        className='
+    </label>
+    <input
+      id='input-search'
+      type='search'
+      name={idSearch}
+      placeholder='Buscar recursos...'
+      value={textSearch}
+      onChange={handleSearch}
+      className='w-full outline-0'
+    />
+    <select
+      id='input-select'
+      name={idCategory}
+      value={selectedCategory}
+      onChange={handleCategory}
+      className='category-select'
+    >
+      {Object.values(Category).map(option => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+    <input
+      type='submit'
+      value='Buscar'
+      className='
             px-3 py-1 text-center border border-border rounded-full
             bg-linear-to-tr from-accent to-tertiary cursor-pointer
             hover:shadow-[-10px_0_20px_var(--color-accent),10px_0_20px_var(--color-tertiary)] transition-all duration-300
           '
-      />
-    </form>
-  </div>
+    />
+  </form>
 }
