b2b11ea55f2c9f6807c046eb2235a8fa7c10e4fb
JaamDev <jaamdev@hotmail.com>
2026-04-12 17:55:03 +0200

    feat(resourceList): Examples of added search queries

--- src/components/resource/ResourceList.tsx
+++ src/components/resource/ResourceList.tsx
@@ -6,6 +6,16 @@ interface Props {
   resources: IResource[]
 }
 
+function AnchorExample({ href }: { href: string }) {
+  return <a
+    href={`?q=${href.toLowerCase()}`}
+    title={`Buscar por ${href}`}
+    className='px-2 py-1 bg-linear-to-tr from-accent to-tertiary rounded hover:to-accent transition-colors duration-300'
+  >
+    {href}
+  </a>
+}
+
 export default function ResourceList({ resources }: Props) {
   const { currentFavorites, saveOne, removeOne } = useLocalStorage()
   const firstTime = localStorage.getItem('prevSearch') ? false : true
@@ -20,7 +30,16 @@ export default function ResourceList({ resources }: Props) {
   }
 
   if (firstTime && resourcesNum === 0) {
-    return <h2 className='size-full px-2 py-1 m-auto mt-14 max-w-6xl text-xl text-center'>¡Hola! 👋<br />¿Listo para mover las manitas?</h2>
+    return <>
+      <h2 className='size-full px-2 py-1 m-auto mt-14 max-w-6xl text-xl text-center'>¡Hola! 👋<br />¿Listo para mover las manitas?</h2>
+      <div className='px-4 py-2 m-auto mt-2 max-w-6xl flex flex-row flex-wrap justify-center items-center gap-2 border-2 border-border bg-secondary rounded-full'>
+        <h3 className='mr-2'>¿ Qué toca hoy ? 👉 </h3>
+        <AnchorExample href='Cursos' />
+        <AnchorExample href='Skills' />
+        <AnchorExample href='SQL' />
+        <AnchorExample href='Deploy' />
+      </div>
+    </>
   } else if (!firstTime && resourcesNum === 0) {
     return <h2 className='size-full px-2 py-1 m-auto mt-14 max-w-6xl text-xl text-center'>¡Vaya!<br />No se encontró nada 😒</h2>
   } else {
